1159c108278583da5c3efd280717d8acb5883a76
Ezequiel <ezrodriguez@fi.uba.ar>
2023-08-25 00:37:34 +0000

    añadido de printf y documentacion
    Co-authored-by: AgustinPessina00 <apessina@fi.uba.ar>

--- main.cpp
+++ main.cpp
@@ -3,15 +3,36 @@
 
 int main()
 {
+    ///
+    /// Se instancia gasDetector como un objeto de la clase DigitalIn correspondiente a la entrada digital D2
+    ///
     DigitalIn gasDetector(D2);
 
+    ///
+    /// Se instancia alarmLed como un objeto de la clase DigitalOut correspondiente al led LED1
+    ///
     DigitalOut alarmLed(LED1);
 
+    ///
+    /// Agrega a la entrada D2 un pulldown
+    ///
     gasDetector.mode(PullDown);
 
+    ///
+    /// Inicializamos el led de alarma como apagado
+    ///
     alarmLed = OFF;
 
+    ///
+    /// Imprimimos por consola la frase Hello World
+    ///
+    printf("%s\n", "Hello World");
+
+    ///
+    /// Ejecuta un loop infinito donde prende el led si detecta un estado alto en la entrada gasDetector, lo apaga si hay un estado bajo
+    ///
     while (true) {
+        
         if ( gasDetector == ON ) {
             alarmLed = ON;
         }
